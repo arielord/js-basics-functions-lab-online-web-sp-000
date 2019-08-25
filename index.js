30959dcf40695a55c62987a281517333905d5c66
Ariel <arielord@buffalo.edu>
2019-08-25 21:15:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+  
+}
+
+function distanceFromHqInFeet(){
+  
+}
+
+function distanceTravelledInFeet(){
+  
+}
+
+function calculatesFarePrices(start, destination){
+  
+}
